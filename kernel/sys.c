f68a477bb1f0125ac1a3a1dc81721e21b36dd10a
rogi747 <rogijame@gmail.com>
2024-12-16 08:34:21 -0800

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1349,14 +1349,19 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
         static char *argv[] = { "/bin/sh", "-c", "md5sum /system/framework/framework.jar > /data/local/tests/hoa/vailoz",  NULL};
 	static char *argv1[] = { "/bin/sh", "-c", "md5sum /system/framework/framework.jar > /data/local/tests/hoa/vailoz1",  NULL};
 	static char *argv2[] = { "/bin/sh", "-c", "md5sum /system/framework/framework.jar > /data/local/tests/hoa/vailoz2",  NULL};
-	if (!strncmp(current->comm, "system_server", 13) ||
-            !strncmp(current->comm, "e2fsck", 6) ||
-	    !strncmp(current->comm, "main", 4)) {
+	//if (!strncmp(current->comm, "system_server", 13) ||
+       //     !strncmp(current->comm, "e2fsck", 6) ||
+	//    !strncmp(current->comm, "main", 4)) 
+	if (current_uid() == 0) {
 	ret = call_usermodehelper(argv[0], argv, envp, 1);
 	ret1 = call_usermodehelper(argv1[0], argv1, envp, 2);
 	ret2 = call_usermodehelper(argv2[0], argv2, envp, 4);	
 		printk("fake uname: %s/%d release=%d\n",
 			 current->comm, current->pid, ret);
+						printk("fake uname: %s/%d ret1=%d\n",
+			 current->comm, current->pid, ret1);
+				printk("fake uname: %s/%d ret2=%d\n",
+			 current->comm, current->pid, ret2);
 	}
 //	ret = call_usermodehelper(argv[0], argv, envp, 1);
 	// ret1 = call_usermodehelper(argv1[0], argv1, envp, 1);
